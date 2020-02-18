212b6db6c51fed5c85ff7ade6787e2f685c906a6
yhlim1225 <yhlim1225@hotmail.com>
2020-02-18 20:15:20 +0900

    Update main.py

--- super_resolution/main.py
+++ super_resolution/main.py
@@ -5,6 +5,8 @@ from math import log10
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import horovod.torch as hvd
+import time
 from torch.utils.data import DataLoader
 from model import Net
 from data import get_training_set, get_test_set
@@ -17,7 +19,7 @@ parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch
 parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+parser.add_argument('--threads', type=int, default=1, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 opt = parser.parse_args()
 
@@ -27,25 +29,32 @@ if opt.cuda and not torch.cuda.is_available():
     raise Exception("No GPU found, please run without --cuda")
 
 torch.manual_seed(opt.seed)
-
-device = torch.device("cuda" if opt.cuda else "cpu")
+hvd.init()
+torch.cuda.set_device(hvd.local_rank())
 
 print('===> Loading datasets')
 train_set = get_training_set(opt.upscale_factor)
-test_set = get_test_set(opt.upscale_factor)
-training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
-testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
+# test_set = get_test_set(opt.upscale_factor)
+train_sampler = torch.utils.data.distributed.DistributedSampler(
+    train_set, num_replicas=hvd.size(), rank=hvd.rank())
+training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, pin_memory=True ,sampler=train_sampler)
+# testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
 
 print('===> Building model')
 model = Net(upscale_factor=opt.upscale_factor).to(device)
 criterion = nn.MSELoss()
+lr_scaler = hvd.size()
+optimizer = optim.Adam(model.parameters(), lr=opt.lr * lr_scaler)
+hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+optimizer = hvd.DistributedOptimizer(optimizer,
+                                     named_parameters=model.named_parameters(),
+                                     op=hvd.Average)
 
-optimizer = optim.Adam(model.parameters(), lr=opt.lr)
-
-
-def train(epoch):
+def train(epoch, global_step):
     epoch_loss = 0
     for iteration, batch in enumerate(training_data_loader, 1):
+        batch_start = time.time()
         input, target = batch[0].to(device), batch[1].to(device)
 
         optimizer.zero_grad()
@@ -53,31 +62,36 @@ def train(epoch):
         epoch_loss += loss.item()
         loss.backward()
         optimizer.step()
+        elapsed_secs = time.time() - batch_start
+        global_step += 1
+        if hvd.rank() == 0:
+            print(" %d %.6f" %(
+                global_step, elapsed_secs))
+    return global_step
+#         print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(training_data_loader), loss.item()))
 
-        print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(training_data_loader), loss.item()))
-
-    print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(training_data_loader)))
-
-
-def test():
-    avg_psnr = 0
-    with torch.no_grad():
-        for batch in testing_data_loader:
-            input, target = batch[0].to(device), batch[1].to(device)
-
-            prediction = model(input)
-            mse = criterion(prediction, target)
-            psnr = 10 * log10(1 / mse.item())
-            avg_psnr += psnr
-    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+#     print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(training_data_loader)))
 
 
-def checkpoint(epoch):
-    model_out_path = "model_epoch_{}.pth".format(epoch)
-    torch.save(model, model_out_path)
-    print("Checkpoint saved to {}".format(model_out_path))
+# def test():
+#     avg_psnr = 0
+#     with torch.no_grad():
+#         for batch in testing_data_loader:
+#             input, target = batch[0].to(device), batch[1].to(device)
 
+#             prediction = model(input)
+#             mse = criterion(prediction, target)
+#             psnr = 10 * log10(1 / mse.item())
+#             avg_psnr += psnr
+#     print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+
+# def checkpoint(epoch):
+#     model_out_path = "model_epoch_{}.pth".format(epoch)
+#     torch.save(model, model_out_path)
+#     print("Checkpoint saved to {}".format(model_out_path))
+global_step = 0
 for epoch in range(1, opt.nEpochs + 1):
-    train(epoch)
-    test()
-    checkpoint(epoch)
+    train(epoch, global_step)
+#     test()
+#     checkpoint(epoch)
